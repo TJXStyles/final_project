b9dc33566739edb95c8a5881a6c7683a788ed3fa
Jack Cooc <jack.cooc@gmail.com>
2013-07-29 13:55:52 -0400

    Create landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FinalProject::Application.routes.draw do
+  get "users/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
