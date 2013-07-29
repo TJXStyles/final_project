c5d32400943f92c9ec5436af95201bbf51dad7a7
Jack Cooc <jack.cooc@gmail.com>
2013-07-29 13:05:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+== Comic Book Collector Database Final Project
+
+This is my final project for Bitmaker Labs. It is a comic book database to store your collected works.
+
+* Search for Comics
+
+* Keep track of your comic books
+
+* User reviews
+
+* Create pull lists and email them to your local comic book shop
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalProject::Application.config.secret_key_base = 'a545f7fa17bbb81866c2d8527e3645d634957985efcdc268ce6f74acc985f21263f2420148541bfb3fc9d5d1e02db64ad9f5ee56b88f7d0faed64496743d16c7'
