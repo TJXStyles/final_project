18a86f269ee6c37df753b1dc1bf61925707c47fa
Jack Cooc <jack.cooc@gmail.com>
2013-07-29 14:08:12 -0400

    migrate users model with email and name, and add my collection link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FinalProject::Application.routes.draw do
   get "users/index"
+  get "users/new"
+
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
